8b983b57391093546746618bcfa38b7327ea4faf
Балашова Ангелина <angelina.balashova@simbirsoft.com>
2020-11-11 15:52:52 +0300

    TASK-1 Add Swagger

---
+++ AspNetLection/Common/Swagger/SwaggerConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetLection.Common.Swagger
+{
+    /// <summary>
+    /// Расширения для конфигурации Swagger.
+    /// </summary>
+    public static class SwaggerConfiguration
+    {
+        /// <summary>
+        /// Настройка документов Swagger.
+        /// </summary>
+        /// <param name="services">Коллекция сервисов для DI.</param>
+        public static void ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerDocument(c =>
+            {
+                c.Title = "Dresses";
+                c.DocumentName = SwaggerDocParts.Dresses;
+                c.ApiGroupNames = new[] { SwaggerDocParts.Dresses };
+            })
+            .AddSwaggerDocument(c =>
+            {
+                c.Title = "Version 2";
+                c.DocumentName = "v2";
+                c.ApiGroupNames = new[] {"v2"};
+            });
+        }
+    }
+}
---
+++ AspNetLection/Common/Swagger/SwaggerDocParts.cs
@@ -0,0 +1,13 @@
+﻿namespace AspNetLection.Common.Swagger
+{
+    /// <summary>
+    /// Константы для указания частей API в Swagger.
+    /// </summary>
+    public sealed class SwaggerDocParts
+    {
+        /// <summary>
+        /// Константа для swagger-документа "Одежда".
+        /// </summary>
+        public const string Dresses = nameof(Dresses);
+    }
+}
