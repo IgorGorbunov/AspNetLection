3207121427ca5a7f77b0611ab8d1c0c710f26239
Балашова Ангелина <angelina.balashova@simbirsoft.com>
2020-11-11 16:52:26 +0300

    TASK-2 Add NLog

--- AspNetLection/Controllers/WeatherForecastController.cs
+++ AspNetLection/Controllers/WeatherForecastController.cs
@@ -28,6 +28,7 @@ namespace AspNetLection.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogInformation("Weather/Get was requested.");
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
